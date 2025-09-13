d941dad7edea2e5fe2638ab68fe8f001b174e74b
HAJIMARU <chonlareeketkorwoing@gmail.com>
2025-09-13 10:07:05 +0700

    add batch file
    add batch file for running simulater program and add .class file

---
+++ RunBatch.bat
@@ -0,0 +1,24 @@
+@echo off
+cd /d "E:\University\3rd_Years\OS\ProcessThreadProject"
+
+echo Compiling .java files...
+javac *.java
+if errorlevel 1 (
+  echo.
+  echo COMPILATION FAILED. Check errors above.
+  pause
+  goto :eof
+)
+
+echo.
+echo Starting PubSub and Nodes...
+
+start "PubSub" cmd /k "java PubSub"
+
+timeout /t 1 >nul
+
+start "Node 1" cmd /k "java Node 1"
+start "Node 2" cmd /k "java Node 2"
+start "Node 3" cmd /k "java Node 3"
+
+exit /b
